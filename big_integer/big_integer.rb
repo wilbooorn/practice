661cc46b3093b068a7501fc4489e3f47833dce08
Robin <rtwilborn@gmail.com>
2017-07-12 10:03:55 -0700

    Implement big integer addition

--- big_integer/big_integer.rb
+++ big_integer/big_integer.rb
@@ -1,19 +1,43 @@
 class BigInteger
-  attr_accessor :num1, :num2
+  attr_accessor :num1, :num2, :max, :min, :store1, :store2
 
   def initialize(num1, num2)
     @num1 = num1
     @num2 = num2
+    @max = num1.length > num2.length ? num1.length : num2.length
+    @min = num1.length < num2.length ? num1.length : num2.length
+    @store1 = Array.new(max, 0)
+    @store2 = Array.new(max, 0)
   end
 
   def add
-    store1 = num1.split("").reverse
-    store2 = num2.split("").reverse
-    p store1
+    idx1 = 0
+    idx2 = 0
+    while idx1 < num1.length
+      @store1[idx1] = num1[num1.length - 1 - idx1].to_i
+      idx1 += 1
+    end
+    while idx2 < num2.length
+      @store2[idx2] = num2[num2.length - 1 - idx2].to_i
+      idx2 += 1
+    end
+
+    carry = 0
+    result = []
+    (0...max).each do |i|
+      current = @store1[i] + @store2[i] + carry
+      current > 9 ? carry = 1 : carry = 0
+      result << current % 10
+    end
+    if carry == 1
+      result << "1"
+    end
+    result = result.map { |el| el.to_s }.join("").reverse
+    p result
   end
 
 end
 
 
-int = BigInteger.new("23456434", "121212121")
+int = BigInteger.new("99999", "11111")
 int.add
