7cf1a9e2a00ec21476eb410af023d03e4941d51f
Robin <rtwilborn@gmail.com>
2017-07-12 16:32:05 -0700

    add test case

--- shuffle/diagonal.rb
+++ shuffle/diagonal.rb
@@ -7,32 +7,34 @@ def diagonal(matrix)
   row = matrix.length
   column = matrix[0].length
   (0...row).each do |idx|
-    puts matrix[idx][0]
+    print "#{matrix[idx][0]} "
     i = idx - 1
     j = 1
     while is_valid?(i, j, matrix)
-      puts matrix[i][j]
+      print "#{matrix[i][j]} "
       i -= 1
       j += 1
     end
+    print "\n"
   end
 
   (1...column).each do |idx|
-    puts matrix[row - 1][idx]
+    print "#{matrix[row - 1][idx]} "
     i = row - 2
     j = idx + 1
     while is_valid?(i, j, matrix)
-      puts matrix[i][j]
+      print "#{matrix[i][j]} "
       i -= 1
       j += 1
     end
+    print "\n"
   end
 end
 
 
-a = [[1,  2,  3,  4],
-     [5,  6,  7,  8],
-     [9,  10, 11, 12],
-     [13, 14, 15, 16]]
+a = [[1,  2,  3,  4, 17],
+     [5,  6,  7,  8, 18],
+     [9,  10, 11, 12, 19],
+     [13, 14, 15, 16, 20]]
 
 diagonal(a)
